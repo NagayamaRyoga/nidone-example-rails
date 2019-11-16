e916f94f5c84bec1adaf80e42a1d77c7b63cb8c1
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-11-16 19:37:35 +0900

    Enable nidone

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,21 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' if ENV["ENABLE_BOOTSNAP"] # Speed up boot time by caching expensive operations.
+(require 'nidone'; Nidone.setup(cache_path: 'tmp/.nidone')) unless ENV.has_key?("ENABLE_BOOTSNAP")
+
+require 'bootsnap'
+
+env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || ENV['ENV']
+development_mode = ['', nil, 'development'].include?(env)
+cache_dir = ENV['BOOTSNAP_CACHE_DIR']
+iseq_cache_enabled = ENV.has_key?("ENABLE_BOOTSNAP")
+
+Bootsnap.setup(
+  cache_dir:            cache_dir,
+  development_mode:     development_mode,
+  load_path_cache:      true,
+  autoload_paths_cache: true, # assume rails. open to PRs to impl. detection
+  disable_trace:        false,
+  compile_cache_iseq:   iseq_cache_enabled,
+  compile_cache_yaml:   true,
+)
